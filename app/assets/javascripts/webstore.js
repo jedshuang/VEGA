d5fb35ddff92c1ed3ef4daa58d9be9ec5f174e8c
Shai Caspin <shai@shais-mbp-2.wireless-1x.unc.edu>
2019-12-11 15:21:33 -0500

    changes to account

--- app/assets/javascripts/webstore.js
+++ app/assets/javascripts/webstore.js
@@ -1,3 +1,20 @@
-export function search_paths() {
-    console.log("hi")
-}
+
+// let r = axios.get('http://localhost:3000/private/DAG');
+// console.log(r);
+
+var availableTags = [
+    "Making a youtube playlist",
+    "How to beat tetris",
+    "How to find memes on reddit",
+    "Waiting for the backend to be ready",
+    "Learning to how to use the extension"
+];
+
+
+
+$("#searchbar").autocomplete({
+    source: availableTags
+  });
+
+    
+
