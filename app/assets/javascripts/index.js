6a13678f132d6e7c29d42f51e43c83644b443c19
Shai Caspin <shai@shais-mbp-2.wireless-1x.unc.edu>
2019-12-11 22:20:44 -0500

    functional webstore

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,9 +4,21 @@ export let myStorage = window.localStorage;
 $(function() {
     let jwt = myStorage.getItem("jwt")
 
-    console.log(jwt);
+    // console.log(jwt);
     if (jwt){
-        console.log("here");
-        $("#login-button").replaceWith('<a href="app/views/logout.html"><button class="button" id="logout-button">logout</button></a>')
+        // console.log("here");
+        $("#login-button").replaceWith('<a href="#"><button class="button" id="logout-button">logout</button></a>')
     }
+
+    $("#logout-button").click(function(){
+        //console.log("hi")
+        let confirmation = confirm("Are you sure?");
+        if (confirmation){
+            localStorage.removeItem('jwt');
+            localStorage.removeItem('username');
+        }
+        $("#logout-button").replaceWith('<a href="#"><button class="button" id="login-button">login</button></a>')
+    });
+
 });
+
--- app/assets/javascripts/webstore.js
+++ app/assets/javascripts/webstore.js
@@ -1,20 +1,61 @@
 
-// let r = axios.get('http://localhost:3000/private/DAG');
-// console.log(r);
+//  var availableTags = ["Making a youtube playlist", "How to beat tetris", "How to find memes on reddit", "How to find ebooks", "How to get into bitcoin mining", "How to start a blog", "How to hack a bad website", "How to make and edit wikipedia pages", "How to buy youtube followers", "How to become a multibillionaire", "How to donload all material needed for COMP401"];
+let availableTags = [""];
 
-var availableTags = [
-    "Making a youtube playlist",
-    "How to beat tetris",
-    "How to find memes on reddit",
-    "Waiting for the backend to be ready",
-    "Learning to how to use the extension"
-];
+async function updatePaths(search){
+  const r = await axios.get('http://localhost:3000/public/DAGs');
+  //console.log(r.data.result)
 
+  for (var path in r.data.result) {
+    availableTags.push(r.data.result[path].name)
+    $("#webstore-table-body").append(`
+      <tr>
+          <th>`+r.data.result[path].name+`</th>
+          <td>`+r.data.result[path].description+`</td>
+          <td>`+Math.round(Math.random(0,100)*100)+`</td>
+          <td>`+r.data.result[path].maker+`</td>
+          <td>2019</td> 
+          <td><a href="#"><button class="explore-button">explore</button></a></td>
+      </tr>`
+    )
+  }
 
+}
 
+async function changePathDisplay(search){
+  const r = await axios.get('http://localhost:3000/public/DAGs');
+  // console.log(r.data.result)
+  $("#webstore-table-body").empty();
+  for (var path in r.data.result) {
+    // $("#webstore-table-body").append( ``)
+    if(r.data.result[path].name.includes(search)){
+      console.log(path)
+      $("#webstore-table-body").append(`
+      <tr>
+          <th>`+r.data.result[path].name+`</th>
+          <td>`+r.data.result[path].description+`</td>
+          <td>`+Math.round(Math.random(0,100)*100)+`</td>
+          <td>`+r.data.result[path].maker+`</td>
+          <td>2019</td> 
+          <td><a href="#"><button class="explore-button">explore</button></a></td>
+      </tr>`
+    )
+    }
+  }
+}
+
+updatePaths()
+// console.log(availableTags);
 $("#searchbar").autocomplete({
     source: availableTags
-  });
+});
+
+$("#search-button").click(function (){
+  let inputb = $("#searchbar").val()
+  // console.log(inputb)
+  changePathDisplay(inputb)
+});
+
 
     
 
